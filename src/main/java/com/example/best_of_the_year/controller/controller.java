46eaf5222420f2041cf2ead4284933b5b704e7ac
PaoloFabris98 <f98paolo@gmail.com>
2025-02-08 13:49:39 +0100

    feat:end

--- src/main/java/com/example/best_of_the_year/controller/controller.java
+++ src/main/java/com/example/best_of_the_year/controller/controller.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.example.best_of_the_year.java.*;
@@ -15,6 +16,7 @@ public class controller {
 
     protected ArrayList<Movies> moviesList = new ArrayList<>();
     protected ArrayList<Songs> songsList = new ArrayList<>();
+    protected String titolo;
 
     @GetMapping("/")
     public String index(Model model) {
@@ -26,10 +28,10 @@ public class controller {
     public String Movies(Model model) {
         if (moviesList.size() <= 0) {
             moviesList.add(new Movies(1, "Inception"));
-            moviesList.add(new Movies(2, "The Dark Knight"));
+            moviesList.add(new Movies(2, "TheDarkKnight"));
             moviesList.add(new Movies(3, "Interstellar"));
-            moviesList.add(new Movies(4, "The Godfather"));
-            moviesList.add(new Movies(5, "Pulp Fiction"));
+            moviesList.add(new Movies(4, "TheGodfather"));
+            moviesList.add(new Movies(5, "PulpFiction"));
         }
         model.addAttribute("films", getBestMovies());
         return "movies";
@@ -38,22 +40,26 @@ public class controller {
     @GetMapping("/Songs")
     public String Songs(Model model) {
         if (songsList.size() <= 0) {
-            songsList.add(new Songs(1, "Bohemian Rhapsody"));
-            songsList.add(new Songs(2, "Stairway to Heaven"));
-            songsList.add(new Songs(3, "Hotel California"));
+            songsList.add(new Songs(1, "BohemianRhapsody"));
+            songsList.add(new Songs(2, "StairwaytoHeaven"));
+            songsList.add(new Songs(3, "HotelCalifornia"));
             songsList.add(new Songs(4, "Imagine"));
-            songsList.add(new Songs(5, "Smells Like Teen Spirit"));
+            songsList.add(new Songs(5, "SmellsLikeTeenSpirit"));
         }
         model.addAttribute("songs", getBestSongs());
         return "songs";
     }
 
-    public String MoviesId(Model model) {
+    @GetMapping("/Movies/{id}")
+    public String MoviesId(@PathVariable String id, Model model) {
+        model.addAttribute(id);
         model.addAttribute("films", getBestMovies());
         return "moviesId";
     }
 
-    public String SongsId(Model model) {
+    @GetMapping("/Songs/{id}")
+    public String SongsId(@PathVariable String id, Model model) {
+        model.addAttribute(id);
         model.addAttribute("songs", getBestSongs());
         return "songsId";
     }
